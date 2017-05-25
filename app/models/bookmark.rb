a93b11819c96efc9f1e53801c50d41f443a5a721
cpfeiffe <cpfeiffe@users.noreply.github.com>
2017-05-25 15:37:21 -0500

    more changes

---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark < ApplicationRecord
+  validates :movie_id, :uniqueness => true
+  validates :user_id, :uniqueness => true
+
+  belongs_to(:movie, :class_name => "Movie", :foreign_key => "movie_id")
+  belongs_to(:user, :class_name => "User", :foreign_key => "user_id")
+
+
+end
